be43953f4882dfacdf4377d1a887ec173e591326
damar-ops <symwalmghrb@gmail.com>
2026-08-03 16:41:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
-// config-front.js - خاص بالموقع فقط
+// config-front.js
+
 export default {
-    API_URL: "https://damar-api-production.up.railway.app",
-    OWNER_NUMBER: "212633226499", // نفس الرقم لي فالبوت
-    BOT_NAME: "DAMAR-MD",
-    TIMEOUT: 60000
-}
+  API_URL: "https://damar-api-production.up.railway.app",
+  OWNER_NUMBER: "212633226499",
+  BOT_NAME: "DAMAR-MD",
+  TIMEOUT: 60000
+};
