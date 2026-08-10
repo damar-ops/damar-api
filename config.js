fe80dd777cc874f8b366978792a59f6e8d76b8d9
damar-ops <symwalmghrb@gmail.com>
2026-08-10 18:06:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212702816500;
+global.pairingNumber = 212603415919;
 global.owner = [
   ['212633226499', 'DAMAR-MD', true],
   ['', 'Owner 10', true],
 ];
 
-global.namebot = 'gaff ai';
+global.namebot = 'بوت';
 global.author = 'DAMAR-MD';
 global.source = 'https://www.facebook.com/profile.php?id=61591783185803';
 
